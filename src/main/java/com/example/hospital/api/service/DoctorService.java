15b33282c43a66c337a03bc38a7dc1986f7aaa37
yuandesheng <18603542180@163.com>
2023-08-07 13:04:30 +0800

    [3.10]--3-10使用RESTful封装查询医生信息接口

--- src/main/java/com/example/hospital/api/service/DoctorService.java
+++ src/main/java/com/example/hospital/api/service/DoctorService.java
@@ -2,8 +2,11 @@ package com.example.hospital.api.service;
 
 import com.example.hospital.api.common.PageUtils;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public interface DoctorService {
     public PageUtils searchByPage(Map param);
+
+    public HashMap searchContent(int id);
 }
